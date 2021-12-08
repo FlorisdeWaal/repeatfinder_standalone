b8d0393b44b8180e433343d19a8f2fa5d5759d59
Floris de Waal <floris.de.waal@gmail.com>
2021-12-07 20:37:04 -0600

    added options to run_rf.py

--- run_rf.py
+++ run_rf.py
@@ -1,19 +1,15 @@
+#imports from std
 import argparse
 import re
-
+#imports from project
 import scorer as sc
 import output_repeatfinder as out
-
-
 
 def populate_known_motifs(keylist):
     outlist = []
     for k in keylist:
         outlist = k.split(":")[-1].strip()
     return outlist
-
-
-
 
 def check_known_classes(seq,known_classes):
     #known classes are checked for by comparing the repeat containing sequences to these regex snippets
@@ -28,7 +24,6 @@ def check_known_classes(seq,known_classes):
             qualdict[key+": "+pattern] = matches
 
     return qualdict
-
 
 def get_known_classes_from_file():
     known_classes = {}
@@ -75,23 +70,22 @@ def find_top_kmer(kmer_dict):
 
     return top_kmer_word,top_kmer_hits 
 
-def create_table(seq,top_kmer_word,top_kmer_hits):
+def create_table(seq,top_kmer_word,top_kmer_hits, cutoff= 2.5):
     table = [top_kmer_word]*len(top_kmer_hits)
     
     cont = True
     while cont:
-        cont = expand_forward(seq,top_kmer_hits,table)
+        cont = expand_forward(seq,top_kmer_hits,table, cutoff)
 
     return table
 
-
-
-
-def expand_forward(seq, hits, pattern_table, cutoff=2.5):
+def expand_forward(seq, hits, pattern_table, cutoff):
 
     next_residue_list = []
     max_len = 15
+
     for i, hit in enumerate(hits):
+
         if hit+len(pattern_table[i]) < len(seq) and len(pattern_table[i]) < max_len:
             next_residue_list.append(seq[hit+len(pattern_table[i])])
         else:
@@ -108,18 +102,21 @@ def expand_forward(seq, hits, pattern_table, cutoff=2.5):
         return False
 
 
-def check_cutoff(nrl, cutoff=0.4):
+def check_cutoff(nrl, cutoff):
 
     max_res = most_common(nrl)
     order, matrix = sc.blosum62()
     score_list = []
+
     for res in nrl:
         current_score = sc.score(max_res, res, order, matrix)
         score_list.append(current_score)
+
     if len(score_list) > 0:
         avg_score = sum(score_list)/len(score_list)
     else:
         return False
+
     if avg_score > cutoff:
         return True
     else:
@@ -158,38 +155,58 @@ def make_pattern(pattern_table):
 
 
 def parse_fasta(iterator):
+
     if not isinstance(iterator, basestring):
         iterator = "\n".join(iterator)
+
     sequence_blocks = [x.strip() for x in iterator.split(">") if x.strip()]
     name = None
     sequences = {}
+
     for sequence_block in sequence_blocks:
+
         sequence_block = sequence_block.splitlines()
         name = sequence_block[0].strip()
         sequence = [x.strip() for x in sequence_block[1:] if x.strip()]
         sequence = "".join(sequence)
         sequences[name] = re.sub("\n","",sequence)
+
     return sequences
-                                                                                                                                                                            
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser("settings for ")
     parser.add_argument('-i',dest='infile',help='path to the input file(fasta format)')
     parser.add_argument('-o',dest= 'output', help='path to the output file')
-
+    parser.add_argument('--maxlen', dest='maxlen', help='set maximum pattern length')
+    parser.add_argument('--cutoff', dest='cutoff', help='set score cutoff for residue comparison')
+    parser.add_argument('--kmer-size', dest='kmer', help='set the size of the initial kmer')
 
     args = parser.parse_args()
+
+    kmer_size = 3
+    cutoff = 2.5
+    max_len = 15
+
+    if args.kmer:
+        kmer_size = args.kmer
+
+    if args.maxlen:
+        max_len = args.maxlen
+    
+    if args.cutoff:
+        cutoff = args.cutoff
 
     with open(args.infile,"r") as infile:
         seqs = parse_fasta(infile) 
     known_motifs = get_known_classes_from_file()
 
     full_output_html = "<div>"
+    full_output_html += "Settings: kmer_size: %s, max_len: %s, cutoff: %s <br>"%(kmer_size, max_len, cutoff)
     for key in seqs:
         km_occurences = check_known_classes(seqs[key],known_motifs)
-        kmer_dict = index_seq(seqs[key])
+        kmer_dict = index_seq(seqs[key], kmer_size)
         top_kmer_word,top_kmer_hits = find_top_kmer(kmer_dict)
-        table = create_table(seqs[key],top_kmer_word,top_kmer_hits)
+        table = create_table(seqs[key],top_kmer_word,top_kmer_hits, cutoff)
 
         output_html = "sequence " + key + "<br>"
         has_km = False
@@ -204,15 +221,15 @@ if __name__ == "__main__":
         if len(km_occurences)>0:
             has_km = True
             should_print = True
-                
-
 
         if has_repeat and has_km:
             pattern = make_pattern(table)
             output_html += "Pattern %s has been found %s times <br>"%(pattern, len(top_kmer_hits))
             output_html += "Instances: %s<br>"%("|".join(table)) 
+            
             for kmkey in km_occurences:
                 output_html += "Known motif %s has been found %s times <br>"%(kmkey, len(km_occurences[kmkey]))
+
             kms = populate_known_motifs(km_occurences.keys())
             output_html += "Sequence:<br>"
             output_html += out.format_linebreaks(out.generate_html_seq(seqs[key],pattern,kms),64)
@@ -222,12 +239,15 @@ if __name__ == "__main__":
             output_html += "Instances: %s<br>"%("|".join(table)) 
             output_html += "Sequence:<br>"
             output_html += out.format_linebreaks(out.generate_html_seq(seqs[key],pattern),64)
+
         elif has_km and not has_repeat:
             for kmkey in km_occurences:
                 output_html += "Known motif %s has been found %s times <br>"%(kmkey, len(km_occurences[kmkey]))
+
             kms = populate_known_motifs(km_occurences.keys())
             output_html += "Sequence:<br>"
             output_html += out.format_linebreaks(out.generate_html_seq(seqs[key],kms),64)
+
         output_html += "<hr>"
         if should_print:
             full_output_html += output_html
